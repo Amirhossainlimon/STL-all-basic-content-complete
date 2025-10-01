93d3aff9399f412caf695e82a268dca27dfe4941
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-10-01 20:03:01 +0600

    Add files via upload

---
+++ list.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    list<int> myList;
+    myList.push_back(5);
+    myList.push_back(15);
+    myList.push_back(25);
+
+    cout << "List: ";
+    for (int x : myList)
+        cout << x << " ";
+    return 0;
+}
+
+
